3f03e2945d270441c27312d33ed1845e75f89288
Mehmet T. AKALIN <makalin@users.noreply.github.com>
2025-04-11 08:51:33 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+# Main application
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Artenticity AI running!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
